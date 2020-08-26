05356defe25cca2671e55bbcb01a4d0b8ca169a7
standlee <57066698@qq.com>
2020-08-26 19:46:14 +0800

    0.3.4

--- simple3D/core/transform.py
+++ simple3D/core/transform.py
@@ -38,12 +38,38 @@ class Transform:
     @property
     def euler(self):
         mat = self.matrix44
-        angle_x = np.arctan2(mat[2, 1], mat[2, 2])
-        angle_y = np.arctan2(-mat[2, 0], np.sqrt(np.square(mat[2, 1]) + np.square(mat[2, 2])))
-        angle_z = np.arctan2(mat[1, 0], mat[0, 0])
+        angle_x = np.arctan2(RM[2, 1], RM[2, 2])
+        angle_y = np.arctan2(-RM[2, 0], np.sqrt(np.square(RM[2, 1]) + np.square(RM[2, 2])))
+        angle_z = np.arctan2(RM[1, 0], RM[0, 0])
         return (angle_x, angle_y, angle_z)
 
     @euler.setter
     def euler(self, euler_angle):
-        euler_matrix44 = pyrr.matrix44.create_from_eulers(euler_angle)
-        self.matrix44[:3, :3] = euler_matrix44[:3, :3]
+        self.matrix44[:3, :3] = euler2RM(euler_angle)
+
+    @property
+    def rotation(self):
+        return self.matrix44[:3, :3]
+    
+    @rotation.setter
+    def rotation(self, mat33):
+        self.matrix44[:3, :3] = mat33
+
+
+def euler2RM(euler):
+    """
+
+    :param euler:
+    :return:
+    """
+    sinP, cosP = np.sin(euler[0]), np.cos(euler[0])
+    sinTheta, cosTheta = np.sin(euler[1]), np.cos(euler[1])
+    sinC, cosC = np.sin(euler[2]), np.cos(euler[2])
+
+    rm_x = np.array([1, 0, 0, 0, cosP, -sinP, 0, sinP, cosP]).reshape((3, 3))
+    rm_y = np.array([cosTheta, 0, sinTheta, 0, 1, 0, -sinTheta, 0, cosTheta]).reshape((3, 3))
+    rm_z = np.array([cosC, -sinC, 0, sinC, cosC, 0, 0, 0, 1]).reshape((3, 3))
+
+    rm_zyx = np.dot(np.dot(rm_z, rm_y), rm_x)
+
+    return rm_zyx
